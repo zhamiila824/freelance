98f87630af78809e0cd607f06c7cf5db60a38a2e
jamilya824 <jama.98.jk@gmail.com>
2019-06-15 00:06:45 +0600

    task model & serializer created

---
+++ task/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from user.models import User
+
+
+class Task(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.PositiveIntegerField()
+    date = models.DateTimeField(auto_now_add=True)
+    done = models.BooleanField(default=False)
+    executor = models.ForeignKey(User, related_name='Executor', on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return '%s    %s' % (self.title, self.price)
---
+++ task/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import generics, status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from . import serializers
+from . import models
+
+
+class TaskListView(generics.ListAPIView):
+    queryset = models.Task.objects.all()
+    serializer_class = serializers.TaskSerializer
+    permission_classes = (AllowAny,)
+
+
+class TaskDetailView(generics.RetrieveAPIView):
+    queryset = models.Task.objects.all()
+    serializer_class = serializers.TaskSerializer
+    permission_classes = (AllowAny,)
+
+
+class TaskCreateView(generics.CreateAPIView):
+    serializer_class = serializers.TaskSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            validated_data = serializer.validated_data
+            models.Task.objects.create(
+                title=validated_data.get('title'),
+                description=validated_data.get('description'),
+                price=validated_data.get('price'),
+                customer=request.user
+            )
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
