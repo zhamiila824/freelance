dfd35207be658b1b7d5f8f85c392071c38bc551b
jamilya824 <jama.98.jk@gmail.com>
2019-07-22 03:00:24 +0600

    tests fixed

--- task/tests.py
+++ task/tests.py
@@ -7,6 +7,7 @@ from user.models import User
 
 
 class TaskTest(TestCase):
+    base_url = 'http://127.0.0.1:8000/api/v1'
 
     def setUp(self):
         self.customer = User.objects.create(
@@ -16,6 +17,9 @@ class TaskTest(TestCase):
             role=0,
             balance=1000
         )
+        self.customer.set_password('12345')
+        self.customer.save(update_fields=['password'])
+
         self.executor = User.objects.create(
             username='executor',
             password='12345',
@@ -23,160 +27,20 @@ class TaskTest(TestCase):
             role=1,
             balance=500
         )
+        self.executor.set_password('12345')
+        self.executor.save(update_fields=['password'])
+
         self.task = Task.objects.create(
             customer=self.customer,
             title='Web site',
             description='Online queue for hospitals',
             price=100
         )
-
-    def test_string_representation(self):
-        task = Task(title="Web", price=500)
-        self.assertEqual(str(task), task.title)
-
-
-class ViewsTest(TestCase):
-    base_url = 'http://127.0.0.1:8000/api/v1'
 
     def test_task_list(self):
         url = '%s/tasks' % self.base_url
         view = self.client.get(url)
         self.assertEqual(view.status_code, status.HTTP_200_OK)
-
-    def test_task_detail_unauthorized(self):
-        url = '%s/tasks/1' % self.base_url
-        view = self.client.get(url)
-        self.assertEqual(view.status_code, status.HTTP_401_UNAUTHORIZED)
-
-    def test_task_detail_customer(self):
-        url = '%s/auth/sign_in' % self.base_url
-        self.user = User.objects.create(
-            username='user',
-            role=0,
-            password='12345',
-        )
-
-        self.user.set_password('12345')
-        self.user.save(update_fields=['password'])
-        data = {
-            'username': 'user',
-            'password': '12345'
-        }
-        self.client.post(url, data)
-        self.customer = User.objects.create(
-            username='customer',
-            password='12345',
-            email="customer@mail.com",
-            role=0,
-            balance=1000
-        )
-        self.task = Task.objects.create(
-            customer=self.customer,
-            title='Web site',
-            description='Online queue for hospitals',
-            price=100
-        )
-        url = '%s/tasks/1' % self.base_url
-        view = self.client.patch(url)
-        self.assertEqual(view.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_task_detail_executor(self):
-        url = '%s/auth/sign_in' % self.base_url
-        self.user = User.objects.create(
-            username='executor',
-            role=1,
-            password='12345',
-        )
-
-        self.user.set_password('12345')
-        self.user.save(update_fields=['password'])
-        data = {
-            'username': 'executor',
-            'password': '12345'
-        }
-        self.client.post(url, data)
-        self.customer = User.objects.create(
-            username='customer',
-            password='12345',
-            email="customer@mail.com",
-            role=0,
-            balance=1000
-        )
-        self.task = Task.objects.create(
-            customer=self.customer,
-            title='Web site',
-            description='Online queue for hospitals',
-            price=100
-        )
-        url = '%s/tasks/1' % self.base_url
-        view = self.client.patch(url)
-        self.assertEqual(view.status_code, status.HTTP_202_ACCEPTED)
-
-    def test_task_detail_done(self):
-        url = '%s/auth/sign_in' % self.base_url
-        self.user = User.objects.create(
-            username='executor',
-            role=1,
-            password='12345',
-        )
-        self.user.set_password('12345')
-        self.user.save(update_fields=['password'])
-        data = {
-            'username': 'executor',
-            'password': '12345'
-        }
-        self.client.post(url, data)
-        self.customer = User.objects.create(
-            username='customer',
-            password='12345',
-            email="customer@mail.com",
-            role=0,
-            balance=1000
-        )
-        self.task = Task.objects.create(
-            customer=self.customer,
-            title='Web site',
-            description='Online queue for hospitals',
-            price=100
-        )
-        url = '%s/tasks/1' % self.base_url
-        self.client.patch(url)
-        view = self.client.patch(url)
-        self.assertEqual(view.status_code, status.HTTP_423_LOCKED)
-
-    def test_task_detail_balance(self):
-        url = '%s/auth/sign_in' % self.base_url
-        self.user = User.objects.create(
-            username='executor',
-            role=1,
-            password='12345',
-        )
-
-        self.user.set_password('12345')
-        self.user.save(update_fields=['password'])
-        data = {
-            'username': 'executor',
-            'password': '12345'
-        }
-        self.client.post(url, data)
-        self.customer = User.objects.create(
-            username='customer',
-            password='12345',
-            email="customer@mail.com",
-            role=0,
-            balance=10000
-        )
-        self.task = Task.objects.create(
-            customer=self.customer,
-            title='Web site',
-            description='Online queue for hospitals',
-            price=1000
-        )
-        self.customer.balance = 100
-        self.customer.save()
-        url = '%s/tasks/1' % self.base_url
-        view = self.client.patch(url)
-        self.assertEqual(view.status_code, status.HTTP_406_NOT_ACCEPTABLE)
 
     def test_create_task_unauthorized(self):
         url = '%s/tasks/add' % self.base_url
@@ -185,14 +49,6 @@ class ViewsTest(TestCase):
 
     def test_create_task_executor(self):
         url = '%s/auth/sign_in' % self.base_url
-        self.user = User.objects.create(
-            username='executor',
-            role=1,
-            password='12345',
-        )
-
-        self.user.set_password('12345')
-        self.user.save(update_fields=['password'])
         data = {
             'username': 'executor',
             'password': '12345'
@@ -209,15 +65,6 @@ class ViewsTest(TestCase):
 
     def test_create_task_balance(self):
         url = '%s/auth/sign_in' % self.base_url
-        self.user = User.objects.create(
-            username='customer',
-            role=0,
-            password='12345',
-            balance=100
-        )
-
-        self.user.set_password('12345')
-        self.user.save(update_fields=['password'])
         data = {
             'username': 'customer',
             'password': '12345'
@@ -227,22 +74,13 @@ class ViewsTest(TestCase):
         data = {
             'title': 'Web site',
             'description': 'Online queue for hospitals',
-            'price': '1000'
+            'price': '10000'
         }
         view = self.client.post(url, data)
         self.assertEqual(view.status_code, status.HTTP_406_NOT_ACCEPTABLE)
 
     def test_create_task(self):
         url = '%s/auth/sign_in' % self.base_url
-        self.user = User.objects.create(
-            username='customer',
-            role=0,
-            password='12345',
-            balance=1000
-        )
-
-        self.user.set_password('12345')
-        self.user.save(update_fields=['password'])
         data = {
             'username': 'customer',
             'password': '12345'
@@ -256,3 +94,55 @@ class ViewsTest(TestCase):
         }
         view = self.client.post(url, data)
         self.assertEqual(view.status_code, status.HTTP_201_CREATED)
+
+    def test_task_detail_unauthorized(self):
+        url = '%s/tasks/1' % self.base_url
+        view = self.client.get(url)
+        self.assertEqual(view.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_task_detail_customer(self):
+        url = '%s/auth/sign_in' % self.base_url
+        data = {
+            'username': 'customer',
+            'password': '12345'
+        }
+        self.client.post(url, data)
+        url = '%s/tasks/1' % self.base_url
+        view = self.client.patch(url)
+        self.assertEqual(view.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_task_detail_executor(self):
+        url = '%s/auth/sign_in' % self.base_url
+        data = {
+            'username': 'executor',
+            'password': '12345'
+        }
+        self.client.post(url, data)
+        url = '%s/tasks/1' % self.base_url
+        view = self.client.patch(url)
+        self.assertEqual(view.status_code, status.HTTP_202_ACCEPTED)
+
+    def test_task_detail_done(self):
+        url = '%s/auth/sign_in' % self.base_url
+        data = {
+            'username': 'executor',
+            'password': '12345'
+        }
+        self.client.post(url, data)
+        url = '%s/tasks/1' % self.base_url
+        self.client.patch(url)
+        view = self.client.patch(url)
+        self.assertEqual(view.status_code, status.HTTP_423_LOCKED)
+
+    def test_task_detail_balance(self):
+        self.customer.balance = 10
+        self.customer.save(update_fields=['balance'])
+        url = '%s/auth/sign_in' % self.base_url
+        data = {
+            'username': 'executor',
+            'password': '12345'
+        }
+        self.client.post(url, data)
+        url = '%s/tasks/1' % self.base_url
+        view = self.client.patch(url)
+        self.assertEqual(view.status_code, status.HTTP_406_NOT_ACCEPTABLE)
