3f87385f5bae5985c6d5e77b0e83e44a4886d7ca
jamilya824 <jama.98.jk@gmail.com>
2019-07-22 03:09:19 +0600

    tests fixed

--- user/tests.py
+++ user/tests.py
@@ -6,35 +6,36 @@ from .models import User
 
 
 class UserTest(TestCase):
+    base_url = 'http://127.0.0.1:8000/api/v1'
 
     def setUp(self):
-        User.objects.create(
+        self.customer = User.objects.create(
             username="user1",
             password="password",
             first_name="user",
             last_name="customer",
-            email="user1@mail.com",
+            email="user1@gmail.com",
             role=0,
             balance=1000
         )
+        self.customer.set_password('password')
+        self.customer.save(update_fields=['password'])
 
-        User.objects.create(
+        self.executor = User.objects.create(
             username="user2",
             password="password",
             first_name="user",
             last_name="executor",
-            email="user2@mail.com",
+            email="user2@gmail.com",
             role=1,
             balance=1000
         )
+        self.executor.set_password('password')
+        self.executor.save(update_fields=['password'])
 
     def test_string_representation(self):
         user = User(username="user", password="123")
         self.assertEqual(str(user), user.username)
-
-
-class ViewsTest(TestCase):
-    base_url = 'http://127.0.0.1:8000/api/v1'
 
     def test_user_list(self):
         url = '%s/users' % self.base_url
@@ -67,43 +68,25 @@ class ViewsTest(TestCase):
         self.assertEqual(view.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_sign_in(self):
-        self.user = User.objects.create(
-            username='test_user',
-            password='12345',
-        )
-        self.user.set_password('12345')
-        self.user.save(update_fields=['password'])
         url = '%s/auth/sign_in' % self.base_url
         data = {
-            'username': 'test_user',
-            'password': '12345'
+            'username': 'user1',
+            'password': 'password'
         }
         view = self.client.post(url, data)
         self.assertEqual(view.status_code, status.HTTP_200_OK)
         self.assertTrue(view.data.get('token'))
 
     def test_sign_in_unauthorized(self):
-        self.user = User.objects.create(
-            username='test_user',
-            password='12345',
-        )
-        self.user.set_password('12345')
-        self.user.save(update_fields=['password'])
         url = '%s/auth/sign_in' % self.base_url
         data = {
-            'username': 'testuser',
-            'password': '12345'
+            'username': 'user',
+            'password': 'password'
         }
         view = self.client.post(url, data)
         self.assertEqual(view.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_sign_in_bad_request(self):
-        self.user = User.objects.create(
-            username='test_user',
-            password='12345',
-        )
-        self.user.set_password('12345')
-        self.user.save(update_fields=['password'])
         url = '%s/auth/sign_in' % self.base_url
         data = {
             'username': 'testuser'
